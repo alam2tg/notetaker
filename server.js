a0983e7edfb0d03e232423b5c50223dedbb27b42
Aaron <aaronlamfinance@gmail.com>
2023-07-29 11:42:22 -0700

    Adjust functions

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const fs = require('fs')
 const util =  require('util')
 const uuid = require('./helpers/uuid')
+const dayjs = require('dayjs')
+
 
 //setup express
 const PORT = 3100;
@@ -13,11 +15,12 @@ app.use(express.json()); // recognizes request object as json object
 app.use(express.urlencoded({ extended: true })); //recognizes data as strings/arrays
 app.use(express.static("public"));
 
-const readFromFile = util.promisify(fs.readFile)  ;
 
-const writeToFile = ('./db/db.json', content) =>
-  fs.writeFile('./db/db.json', JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${'./db/db.json'}`)
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
 const readAndAppend = (content, file) => {
@@ -40,10 +43,22 @@ app.get("/api/notes", (req, res) => {
 	readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
 });
 
+//use post method to call on readAndAppend, post to db.
 app.post("/api/notes", (req, res) => {
 	console.info(`${req.method} request received to add note`);
+
+	const { title, text } = req.body;
+	if (req.body) {
+		const newNote = {
+			title,
+			text,
+			note_id: uuid(),
+		}
+	}
 	
 })
+
+//  Get route for homepage - * any 'wildcards' (non-specified-routes) will be directed to the homepage.
 
 app.get("*", (req, res) =>
 	res.sendFile(path.join(__dirname, "./public/index.html"))
